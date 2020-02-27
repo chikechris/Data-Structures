e8c5da205e6db2022b3270fe62a136c194495dfc
chikechris <cchike2017@gmail.com>
2020-02-27 16:56:01 -0500

    updated binary_search_tree, test ran ok

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('./queue_and_stack')
-from dll_queue import Queue
 from dll_stack import Stack
+from dll_queue import Queue
 
 
 class BinarySearchTree:
@@ -10,65 +10,101 @@ class BinarySearchTree:
         self.left = None
         self.right = None
 
+    # Insert the given value into the tree
     def insert(self, value):
-        if self.value > value:
-            if self.left == None:
+      
+        if value < self.value:
+            if not self.left:
                 self.left = BinarySearchTree(value)
             else:
-                return self.left.insert(value)
-
-        if self.value <= value:
-            if self.right == None:
+                self.left.insert(value)
+        else:
+            if not self.right:
                 self.right = BinarySearchTree(value)
             else:
-                return self.right.insert(value)
+                self.right.insert(value)
+
+    # Return True if the tree contains the value
+    # False if it does not
 
     def contains(self, target):
+    
         if target == self.value:
             return True
-        elif target > self.value:
-            if self.right != None:
-                return self.right.contains(target)
-            else:
+        if target < self.value:
+            if not self.left:
                 return False
-
-        elif target < self.value:
-            if self.left != None:
+            else:
                 return self.left.contains(target)
-            else:
+        else:
+            if not self.right:
                 return False
+            else:
+                return self.right.contains(target)
+
+    # Return the maximum value found in the tree
 
     def get_max(self):
-        if self.right != None:
-            return self.right.get_max()
-        else:
+        if not self.right:
             return self.value
+        else:
+            return self.right.get_max()
 
+    # Call the function `cb` on the value of each node
+    # You may use a recursive or iterative approach
     def for_each(self, cb):
-
+        #use a callback to loop through the values
         cb(self.value)
-        if self.right:
-            self.right.for_each(cb)
+        #use callback to loop through left side
         if self.left:
             self.left.for_each(cb)
+        #use callback to loop through right side
+        if self.right:
+            self.right.for_each(cb)
 
     # DAY 2 Project -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
+
     def in_order_print(self, node):
-        pass
+        if node:
+            self.in_order_print(node.left)
+            print(node.value)
+            self.in_order_print(node.right)
+        else:
+            return
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
+
     def bft_print(self, node):
-        pass
+        #initialize a queue
+        queue = Queue()
+        queue.enqueue(node)
+        while queue.len() > 0:
+           popped = queue.dequeue()
+           print(popped.value)
+           if popped.left:
+               queue.enqueue(popped.left)
+           if popped.right:
+               queue.enqueue(popped.right)
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
-    def dft_print(self, node):
-        pass
 
+    def dft_print(self, node):
+        stack = Stack()
+        stack.push(node)
+        while stack.len() > 0:
+            popped = stack.pop()
+            print(popped.value)
+            if popped.left:
+                stack.push(popped.left)
+            if popped.right:
+                stack.push(popped.right)
+
+                
     # STRETCH Goals -------------------------
     # Note: Research may be required
 
